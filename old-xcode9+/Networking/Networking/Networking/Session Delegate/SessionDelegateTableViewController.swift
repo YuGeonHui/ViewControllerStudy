15eca6e0436514675bc981341e371ad5ef2e0a46
YuGeonHui <ghyu_0906@naver.com>
2022-08-22 22:05:39 +0900

    URLSession Delegate Study

--- old-xcode9+/Networking/Networking/Networking/Session Delegate/SessionDelegateTableViewController.swift
+++ old-xcode9+/Networking/Networking/Networking/Session Delegate/SessionDelegateTableViewController.swift
@@ -23,58 +23,115 @@
 import UIKit
 
 class SessionDelegateTableViewController: UITableViewController {
-   
-   @IBOutlet weak var titleLabel: UILabel!
-   @IBOutlet weak var descLabel: UILabel!
-   
-   var session: URLSession!
-   
-   // Code Input Point #3
-   
-   // Code Input Point #3
-   
-   @IBAction func sendReqeust(_ sender: Any) {
-      guard let url = URL(string: "https://kxcoding-study.azurewebsites.net/api/books/3") else {
-         fatalError("Invalid URL")
-      }
-      
-      // Code Input Point #1
-      
-      // Code Input Point #1
-   }
-   
-   override func viewWillDisappear(_ animated: Bool) {
-      super.viewWillDisappear(animated)
-      
-      // Code Input Point #6
-      
-      // Code Input Point #6
-   }
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descLabel: UILabel!
+    
+    var session: URLSession!
+    
+    // Code Input Point #3
+    var buuffer: Data?
+    // Code Input Point #3
+    
+    @IBAction func sendReqeust(_ sender: Any) {
+        
+        guard let url = URL(string: "https://kxcoding-study.azurewebsites.net/api/books/3") else {
+            fatalError("Invalid URL")
+        }
+        
+        // Code Input Point #1
+        let configuration = URLSessionConfiguration.default
+        session = URLSession(configuration: configuration, delegate: self, delegateQueue: OperationQueue.main)
+        
+        buuffer = Data()
+        
+        let task = session.dataTask(with: url)
+        task.resume()
+        // Code Input Point #1
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Code Input Point #6
+        // Session 사용 이후, 리소스 정리(강한 참조 -> 메모리 릭 발생)
+        // session.finishTasksAndInvalidate()
+        session.invalidateAndCancel()
+        // Code Input Point #6
+    }
 }
 
 // Code Input Point #2
-
+extension SessionDelegateTableViewController: URLSessionDataDelegate {
+    
+    // MARK: 서버로부터 최초로 응답을 받았을 때 (응답 값 : 3번째 파라미터!)
+    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive response: URLResponse, completionHandler: @escaping (URLSession.ResponseDisposition) -> Void) {
+        
+        guard let response = response as? HTTPURLResponse, (200...299).contains(response.statusCode)  else {
+            completionHandler(.cancel)
+            return
+        }
+        
+        completionHandler(.allow)
+    }
+    
+    // MARK: 서버에서 데이터가 전송될때 마다 반복적으로 발송된다.
+    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
+        
+        buuffer?.append(data)
+    }
+    
+    
+    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
+        if let error = error {
+            showErrorAlert(with: error.localizedDescription)
+        } else {
+            parse()
+        }
+    }
+}
 // Code Input Point #2
 
 extension SessionDelegateTableViewController {
-   func parse() {
-      // Code Input Point #4
-      
-      // Code Input Point #4
-      
-      let decoder = JSONDecoder()
-      
-      decoder.dateDecodingStrategy = .custom({ (decoder) -> Date in
-         let container = try decoder.singleValueContainer()
-         let dateStr = try container.decode(String.self)
-         
-         let formatter = ISO8601DateFormatter()
-         formatter.formatOptions = [.withFullDate, .withTime, .withDashSeparatorInDate, .withColonSeparatorInTime]
-         return formatter.date(from: dateStr)!
-      })
-      
-      // Code Input Point #5
-      
-      // Code Input Point #5
-   }
+    func parse() {
+        
+        // Code Input Point #4
+        guard let data = buuffer else {
+            fatalError("Invalid Buffer")
+        }
+        // Code Input Point #4
+        
+        let decoder = JSONDecoder()
+        
+        decoder.dateDecodingStrategy = .custom({ (decoder) -> Date in
+            let container = try decoder.singleValueContainer()
+            let dateStr = try container.decode(String.self)
+            
+            let formatter = ISO8601DateFormatter()
+            formatter.formatOptions = [.withFullDate, .withTime, .withDashSeparatorInDate, .withColonSeparatorInTime]
+            return formatter.date(from: dateStr)!
+        })
+        
+        // Code Input Point #5
+        do {
+            
+            let detail = try decoder.decode(BookDetail.self, from: data)
+            
+            if detail.code == 200 {
+                titleLabel.text = detail.book.title
+                descLabel.text = detail.book.desc
+                
+                tableView.reloadData()
+                
+            } else {
+                showErrorAlert(with: detail.message ?? "Error")
+            }
+            
+            
+        } catch {
+            showErrorAlert(with: error.localizedDescription)
+            print(error)
+        }
+        // Code Input Point #5
+    }
 }
