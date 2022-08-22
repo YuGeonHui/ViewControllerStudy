1d5ad91ec40ace9c3216fdd0224a5bcbee2590c9
YuGeonHui <ghyu_0906@naver.com>
2022-08-22 22:15:49 +0900

    URLSession Configuration

--- old-xcode9+/Networking/Networking/Networking/SessionConfiguration/SessionConfigurationViewController.swift
+++ old-xcode9+/Networking/Networking/Networking/SessionConfiguration/SessionConfigurationViewController.swift
@@ -23,66 +23,82 @@
 import UIKit
 
 class SessionConfigurationViewController: UIViewController {
-   
-   @IBAction func useSharedConfiguration(_ sender: Any) {
-      // Code Input Point #1
-      
-      // Code Input Point #1
-   }
-   
-   @IBAction func useDefaultConfiguration(_ sender: Any) {
-      // Code Input Point #2
-      
-      // Code Input Point #2
-   }
-   
-   @IBAction func useEphemeralConfiguration(_ sender: Any) {
-      // Code Input Point #3
-      
-      // Code Input Point #3
-   }
-   
-   @IBAction func useBackgroundConfiguration(_ sender: Any) {
-      // Code Input Point #4
-      
-      // Code Input Point #4
-   }
-   
-   @IBAction func useCustomConfiguration(_ sender: Any) {
-      // Code Input Point #5
-      
-      // Code Input Point #5
-   }
-   
-   func sendReqeust(using session: URLSession) {
-      guard let url = URL(string: "https://kxcoding-study.azurewebsites.net/api/string") else {
-         fatalError("Invalid URL")
-      }
-      
-      let task = session.dataTask(with: url) { (data, response, error) in
-         if let error = error {
-            self.showErrorAlert(with: error.localizedDescription)
-            print(error)
-            return
-         }
-         
-         guard let httpResponse = response as? HTTPURLResponse else {
-            self.showErrorAlert(with: "Invalid Response")
-            return
-         }
-         
-         guard (200...299).contains(httpResponse.statusCode) else {
-            self.showErrorAlert(with: "\(httpResponse.statusCode)")
-            return
-         }
-         
-         guard let data = data, let str = String(data: data, encoding: .utf8) else {
-            fatalError("Invalid Data")
-         }
-         
-         self.showInfoAlert(with: str)
-      }
-      
-      task.resume()
-   }
+    
+    @IBAction func useSharedConfiguration(_ sender: Any) {
+        // Code Input Point #1
+        sendReqeust(using: URLSession.shared)
+        // Code Input Point #1
+    }
+    
+    @IBAction func useDefaultConfiguration(_ sender: Any) {
+        // Code Input Point #2
+        let configuration = URLSessionConfiguration.default
+        let session = URLSession(configuration: configuration)
+        
+        sendReqeust(using: session)
+        // Code Input Point #2
+    }
+    
+    @IBAction func useEphemeralConfiguration(_ sender: Any) {
+        // Code Input Point #3
+        let configuration = URLSessionConfiguration.ephemeral
+        let session = URLSession(configuration: configuration)
+        
+        sendReqeust(using: session)
+        // Code Input Point #3
+    }
+    
+    @IBAction func useBackgroundConfiguration(_ sender: Any) {
+        // Code Input Point #4
+        let configuration = URLSessionConfiguration.background(withIdentifier: "DownTask")
+        let session = URLSession(configuration: configuration)
+        
+        sendReqeust(using: session)
+        // Code Input Point #4
+    }
+    
+    @IBAction func useCustomConfiguration(_ sender: Any) {
+        // Code Input Point #5
+        let configuration = URLSessionConfiguration.default
+        
+        configuration.timeoutIntervalForRequest = 30
+        configuration.httpAdditionalHeaders = ["ZUMO-API-VERSION" : "2.0.0"]
+        configuration.networkServiceType = .responsiveData
+        
+        let session = URLSession(configuration: configuration)
+        sendReqeust(using: session)
+        // Code Input Point #5
+    }
+    
+    func sendReqeust(using session: URLSession) {
+        guard let url = URL(string: "https://kxcoding-study.azurewebsites.net/api/string") else {
+            fatalError("Invalid URL")
+        }
+        
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                self.showErrorAlert(with: error.localizedDescription)
+                print(error)
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse else {
+                self.showErrorAlert(with: "Invalid Response")
+                return
+            }
+            
+            guard (200...299).contains(httpResponse.statusCode) else {
+                self.showErrorAlert(with: "\(httpResponse.statusCode)")
+                return
+            }
+            
+            guard let data = data, let str = String(data: data, encoding: .utf8) else {
+                fatalError("Invalid Data")
+            }
+            
+            self.showInfoAlert(with: str)
+        }
+        
+        task.resume()
+    }
 }
