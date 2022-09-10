33cb3298a87bcc1a73760c74fa83925bebef6a79
YuGeonHui <ghyu_0906@naver.com>
2022-09-10 19:20:27 +0900

    munu practice

--- xcode12+/AutoLayout/Constraints/Constraints/Practices/MenuViewController.swift
+++ xcode12+/AutoLayout/Constraints/Constraints/Practices/MenuViewController.swift
@@ -25,8 +25,39 @@ import UIKit
 
 class MenuViewController: UIViewController {
 
+    @IBOutlet weak var leftCenterXConstraint: NSLayoutConstraint!
     
+    @IBOutlet weak var leftEqualWidthConstraint: NSLayoutConstraint!
     
+    @IBOutlet weak var rightCenterXContraints: NSLayoutConstraint!
+    
+    @IBOutlet weak var rightEqualWidthContraints: NSLayoutConstraint!
+    
+    @IBAction func selectLeft(_ sender: Any) {
+        
+        leftCenterXConstraint.priority = .required
+        leftEqualWidthConstraint.priority = .required
+        
+        rightCenterXContraints.priority = .defaultHigh // 750
+        rightEqualWidthContraints.priority = .defaultHigh
+        
+        UIView.animate(withDuration: 0.3) {
+            self.view.setNeedsLayout()
+        }
+    }
+    
+    @IBAction func selectRight(_ sender: Any) {
+        
+        leftCenterXConstraint.priority = .defaultHigh
+        leftEqualWidthConstraint.priority = .defaultHigh
+        
+        rightCenterXContraints.priority = .required
+        rightEqualWidthContraints.priority = .required
+        
+        UIView.animate(withDuration: 0.3) {
+            self.view.setNeedsLayout()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
